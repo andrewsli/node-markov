e5bde8291b88b8553f0c42f174e6518b03bc70c9
Andrew Li <andrewli0725@gmail.com>
2019-07-09 11:17:15 -0700

    step-2

--- markov.js
+++ markov.js
@@ -8,7 +8,8 @@ class MarkovMachine {
   constructor(text) {
     let words = text.split(/[ \r\n]+/);
     this.words = words.filter(c => c !== "");
-    this.makeChains();
+    this.wordChain = this.makeChains();
+    
   }
 
   /** set markov chains:
@@ -17,13 +18,64 @@ class MarkovMachine {
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
   makeChains() {
-    // TODO
+    let wordChain = {}
+    for(let i = 0; i < this.words.length; i++){
+      // creates key for word if doesn't exist yet
+      if(!wordChain[this.words[i]]){
+        wordChain[this.words[i]] = []
+      }
+      // add next word into chain for current word, null if last word
+      if((i === this.words.length-1) || this.words[i+1] === "."){
+        wordChain[this.words[i]].push(null)
+      }
+      else{
+        wordChain[this.words[i]].push(this.words[i+1])
+      }
+    }
+    return wordChain
   }
 
 
   /** return random text from chains */
 
   makeText(numWords = 100) {
-    // TODO
+    if (numWords === 0) {
+      return;
+    }
+
+    let text = "";
+    let lastWord = "";
+    
+    let words = Object.keys(this.wordChain);
+    lastWord = randomElement(words);
+    text += capitalize(lastWord) + " ";
+    let counter = 1;
+    
+    while (counter < numWords) {
+      lastWord = randomElement(this.wordChain[lastWord]);
+      if (lastWord === null) {
+        text = text.slice(0,-1);
+        break;
+      }
+      text += lastWord + " ";
+      counter++;
+    }
+    return text;
   }
 }
+
+
+
+// returns random element of array
+function randomElement(array){
+  return array[Math.floor(Math.random() * array.length)]
+}
+
+// return captialized word
+function capitalize(word){
+  return word.charAt(0).toUpperCase() + word.slice(1);
+}
+
+let mm = new MarkovMachine("Hi my name is Billy. Billy likes Billy. Trains are  fun because Bart is fun.")
+mm.wordChain
+
